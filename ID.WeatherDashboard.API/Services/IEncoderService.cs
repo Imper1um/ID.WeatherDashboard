33e411dee3d5e012e3b98ac86b55679ade190dbf
Imper1um <github@imper1umdesigns.com>
2025-07-20 15:40:42 -0400

    Adding in ChatGPT functionality.

---
+++ ID.WeatherDashboard.API/Services/IEncoderService.cs
@@ -0,0 +1,16 @@
+﻿using ID.WeatherDashboard.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID.WeatherDashboard.API.Services
+{
+    public interface IEncoderService
+    {
+        Task<ImageWeatherData?> GetCurrentImageWeatherDataAsync(string path);
+        Task EncodeWeatherDataAsync(string path, ImageWeatherData data);
+        Task<ImageWeatherData?> GenerateImageWeatherDataAsync(string path);
+    }
+}
